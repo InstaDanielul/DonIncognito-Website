64cbaa0df46470542574e591bea00e7459b66fdf
BuildTools <unconfigured@null.spigotmc.org>
2022-08-20 23:35:52 +0300

    Mint

--- backend/src/config.js
+++ backend/src/config.js
@@ -13,7 +13,7 @@ const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 const layerConfigurations = [
   {
-    growEditionSizeTo: 50,
+    growEditionSizeTo: 7777,
     layersOrder: [
       { name: "BACKGROUND", number: 10  },
       { name: "GUARDIAN", number: 40  },
@@ -59,10 +59,10 @@ const OWNER_ADDRESS = '0xD27562F384591Fc230F4489B36328fE2c5504059';
 const TREASURY_ADDRESS = '0xD27562F384591Fc230F4489B36328fE2c5504059';
 const MAX_SUPPLY = 7777; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 100; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-08-21T14:30:00+02:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-08-21T16:30:00+02:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
@@ -70,7 +70,7 @@ const ROYALTY_SHARE = 500; // Percentage of the token price that goes to the roy
 const ROYALTY_ADDRESS = "0xD27562F384591Fc230F4489B36328fE2c5504059"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0xD27562F384591Fc230F4489B36328fE2c5504059","0xD27562F384591Fc230F4489B36328fE2c5504059","0xD27562F384591Fc230F4489B36328fE2c5504059","0xD27562F384591Fc230F4489B36328fE2c5504059","0xD27562F384591Fc230F4489B36328fE2c5504059","0xD27562F384591Fc230F4489B36328fE2c5504059","0xD27562F384591Fc230F4489B36328fE2c5504059","0xD27562F384591Fc230F4489B36328fE2c5504059","0xD27562F384591Fc230F4489B36328fE2c5504059"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
@@ -78,8 +78,8 @@ let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually inclu
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "Ready to get anonymous?"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC_DESCRIPTION = "Ready to get anonymous into the Dark Web and the Metaverse ?"; // Replace with what you want the generic descriptions to say.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafkreib76ensacsacmxwy2x5oyvyorkq74kpxts2nkpo3xqeuusk4olyve"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
